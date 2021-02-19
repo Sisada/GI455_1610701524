5246bb8ad85ffce994fdd8b0e91dcc8d19279a53
DESKTOP-UKCD6UJ\J <Sisada.apir@bumail.net>
2021-02-19 23:49:51 +0700

    Send Assignment Week4

---
+++ GI455_Project/GI455/Assets/Assignment_Week3_RoomChat/Scripts/LoginRoomConnection.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using WebSocketSharp;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+namespace RoomWebSocket
+{
+    public class LoginRoomConnection : MonoBehaviour
+    {
+        [SerializeField]
+        private Text ipConnection;
+        [SerializeField]
+        private Text portConnection;
+
+        private WebSocket webSocket;
+
+        public void ConnectToServerRoom()
+        {
+            if (ipConnection.text == "127.0.0.1" && portConnection.text == "8080")
+            {
+                SceneManager.LoadScene("LobbyRoom");
+            }
+        }
+    }
+}
